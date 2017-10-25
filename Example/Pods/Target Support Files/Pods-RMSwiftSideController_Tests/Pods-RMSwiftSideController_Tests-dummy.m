8e3f2b0afe230c5952374dd301f3b6b4ee0fa200
Master <Master@ChendydeiMac.local>
2017-10-25 16:06:20 +0800

    调整代码

--- Example/Pods/Target Support Files/Pods-RMSwiftSideController_Tests/Pods-RMSwiftSideController_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RMSwiftSideController_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_RMSwiftSideController_Tests
-@end
--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -1,6 +1,6 @@
 import UIKit
 import XCTest
-import RMSwiftSideController
+import Pods_RMSwiftSideController_Example
 
 class Tests: XCTestCase {
     
